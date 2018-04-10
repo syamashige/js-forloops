e9ccaec5f001ecbf28f6976d7f90dde0e0076b73
Sarah <yamashige.sarah@gmail.com>
2018-04-09 22:09:03 -1000

    Finished exercises 10.

--- exercises.js
+++ exercises.js
@@ -185,6 +185,17 @@ Write a function named `copyArray` which takes two arguments: `originArray` and 
 Inside of this function write a for-loop that will iterate through the contents of the `originArray` and pushes each element of that array into `destinationArray`. Console.log your result.
 */
 
+var valuesArray = [99, 66, 829, 1941, 8, 76];
+var copyValuesArray = [];
+
+function copyArray (originArray, destinationArray) {
+  for (var i = 0; i < originArray.length; i++) {
+    destinationArray.push(originArray[i]);
+  }
+}
+copyArray(valuesArray, copyValuesArray);
+console.log(copyValuesArray);
+
 
 
 /*Final Boss*/
