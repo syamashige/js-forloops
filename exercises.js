56aaedcbab224f83abadeb336531799ee1247dfc
Sarah <yamashige.sarah@gmail.com>
2018-04-09 21:43:33 -1000

    Finished exercise 7.

--- exercises.js
+++ exercises.js
@@ -120,6 +120,15 @@ Note that every odd index value in `oopsArray` is currently `undefined`. Using a
 [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
 */
 
+var oopsArray = ["turn", , "down", , "for", , "what"];
+
+for (var i = 0; i < oopsArray.length; i++) {
+  if (oopsArray[i] === undefined) {
+    oopsArray[i] = "nope";
+  }
+}
+console.log(oopsArray);
+
 
 /* 8) Is It There Oops
 Using a for-loop, iterate through the Array stored at `oopsArray` backwards. Console.log your result. It should look like this:
