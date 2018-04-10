70eb3ce3c19295ea2bfdce60555dc06f4efcab45
Sarah <yamashige.sarah@gmail.com>
2018-04-09 21:31:51 -1000

    Finished exercise 4.

--- exercises.js
+++ exercises.js
@@ -72,6 +72,12 @@ Declare a variable named `stringOfNumbers` and assign its value to an empty stri
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
+var stringOfNumbers = "";
+
+for (var i = 10; i <= 20; i++) {
+  stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
